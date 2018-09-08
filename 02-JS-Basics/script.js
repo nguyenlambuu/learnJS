673b3b9c728453d237dcd4fa6ae5dd86a4a6fa5e
buunguyenlam <nguyenlambuu@gmail.com>
2018-09-08 14:20:38 +0700

    Variable mutation and type coercion

--- 02-JS-Basics/script.js
+++ 02-JS-Basics/script.js
@@ -1,5 +1,12 @@
+/**************************
+ * Hello World!!!
+***************************/
 // console.log('Hello World');
 
+/**************************
+ * Variables and data types
+***************************/
+/*
 // String
 var firstName = 'Buu';
 console.log(firstName); // --> Buu
@@ -17,3 +24,26 @@ var job;
 console.log(job); // --> undefined
 
 var years = 2018;
+*/
+
+/**************************************
+ * Variable mutation and type coercion
+***************************************/
+
+var firstName = 'Buu';
+var age = 24;
+
+// Type coercion
+console.log(firstName + ' ' + age); // convert number to string.
+
+var job, isMarried;
+job = 'developer';
+isMarried = false;
+
+console.log(firstName + ' is a ' + age + ' year old ' + job + '. Is he married? ' + isMarried); // convert boolean to string.
+
+// Variable mutation
+age = 'twenty four';
+job = 'driver';
+
+alert(firstName + ' is a ' + age + ' year old ' + job + '. Is he married? ' + isMarried); 
