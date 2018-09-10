1b5a67b450ac1c1cff4a4954d564ff113f858e92
buunguyenlam <nguyenlambuu@gmail.com>
2018-09-10 22:03:57 +0700

    Coding challenge 3

--- 02-JS-Basics/script.js
+++ 02-JS-Basics/script.js
@@ -249,13 +249,12 @@ if (age < 13){
 //     default: console.log(firstName + ' is a man.');
 // }
 
-
 /**********************
  * Truthy and Falsy values and equality operators
  ***********************/
 
- // Falsy values: undefined, null, 0, '', NaN
- // Truthy values: NOT FALSY VALUES
+// Falsy values: undefined, null, 0, '', NaN
+// Truthy values: NOT FALSY VALUES
 /*
  var height;
  console.log(typeof height); // --> undefined
@@ -348,7 +347,7 @@ if(scoreJohn > scoreMike && scoreJohn > scoreMarry){
  yearUntilRetirement(1969, 'Jane');
  */
 
- /**********************
+/**********************
  * Function Statements and Expressions
  ***********************/
 /*
@@ -374,7 +373,7 @@ if(scoreJohn > scoreMike && scoreJohn > scoreMarry){
  console.log(whatDoYouDo('others', 'Doe'));
  */
 
-  /**********************
+/**********************
  * Arrays
  ***********************/
 /*
@@ -409,4 +408,49 @@ console.log(buu.indexOf('designer')); // return -1 because it's not existed
 
 buu.indexOf('designer') === -1 ? console.log('Buu is NOT a designer')
 : console.log('Buu IS a designer');
+*/
+
+/**********************
+ * CODING CHALLENGE 3
+ ***********************/
+/*
+John and his family went on a holiday and went to 3 different restaurants. The bills were $124, $48 and $268.
+To tip the waiter a fair amount, John created a simple tip calculator (as a function). He likes to tip 20% of the bill when the bill is less than $50, 15% when the bill is between $50 and $200, and 10% if the bill is more than $200.
+In the end, John would like to have 2 arrays:
+1) Containing all three tips (one for each bill)
+2) Containing all three final paid amounts (bill + tip).
+(NOTE: To calculate 20% of a value, simply multiply it with 20/100 = 0.2)
+GOOD LUCK 😀
+*/
+/*
+function tipCalculator(bill) {
+	var percentage;
+	switch (true) {
+		case bill < 50:
+			percentage = 0.2;
+			break;
+		case bill >= 50 && bill < 200:
+			percentage = 0.15;
+			break;
+		case bill > 200:
+			percentage = 0.1;
+			break;
+	}
+	return percentage * bill;
+}
+
+var bills = [124, 48, 268];
+var tips = [
+	tipCalculator(bills[0]),
+	tipCalculator(bills[1]),
+	tipCalculator(bills[2])
+];
+
+var finalValues = [
+    bills[0] + tips[0],
+    bills[1] + tips[1],
+    bills[2] + tips[2]
+]
+
+console.log(finalValues);
 */
