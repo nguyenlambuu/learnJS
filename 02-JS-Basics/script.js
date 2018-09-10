5d2c8a6528538dcbf9d772dc13b183aeeb0959db
buunguyenlam <nguyenlambuu@gmail.com>
2018-09-10 21:34:55 +0700

    Function Statements and Expressions

--- 02-JS-Basics/script.js
+++ 02-JS-Basics/script.js
@@ -347,3 +347,29 @@ if(scoreJohn > scoreMike && scoreJohn > scoreMarry){
  yearUntilRetirement(1948, 'Mike');
  yearUntilRetirement(1969, 'Jane');
  */
+
+ /**********************
+ * Function Statements and Expressions
+ ***********************/
+/*
+ // Function declaration
+// function whatDoYouDo(job, firstName) {}
+
+ // Function expresstion
+ var whatDoYouDo = function (job, firstName) {
+     switch (job){
+         case 'teacher': return firstName + ' teaches kids how to code';
+
+         case 'driver': return firstName + ' drives a Grab in SG';
+
+         case 'designer': return firstName + ' designs beautiful websites';
+
+         default: return firstName + ' does something else';
+     }
+ }
+
+ console.log(whatDoYouDo('driver', 'Buu'));
+ console.log(whatDoYouDo('teacher', 'Mike'));
+ console.log(whatDoYouDo('designer', 'Jane'));
+ console.log(whatDoYouDo('others', 'Doe'));
+ */
