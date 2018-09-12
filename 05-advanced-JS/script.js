c58c42570b2e24654ac10e471fe4c18365038ed8
nguyenlambuu <nguyenlambuu@gmail.com>
2018-09-12 16:39:06 +0700

    Function constructor

---
+++ 05-advanced-JS/script.js
@@ -0,0 +1,35 @@
+/**********************
+ * Function constructor
+ ***********************/
+/*
+var john = {
+    name: 'John',
+    yearOfBirth: 1990,
+    job: 'teacher'
+};
+
+var Person = function(name, yearOfBirth, job) {
+    this.name = name;
+    this.yearOfBirth = yearOfBirth;
+    this.job = job;
+}
+
+
+Person.prototype.calculateAge = function (){
+    console.log(2018 - this.yearOfBirth);
+}
+
+Person.prototype.lastName = 'Smith';
+
+var mike = new Person('Mike', 1944, 'designer');
+var jane = new Person('Jane', 1958, 'teacher');
+var bob = new Person('Bob', 1993, 'coder');
+
+mike.calculateAge();
+jane.calculateAge();
+bob.calculateAge();
+
+console.log(mike.lastName);
+console.log(jane.lastName);
+console.log(bob.lastName);
+*/
