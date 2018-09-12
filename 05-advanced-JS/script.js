7be5f27f224461b9ac1e20a56b8294f3275c335b
nguyenlambuu <nguyenlambuu@gmail.com>
2018-09-12 21:35:04 +0700

    Primitive vs Object

--- 05-advanced-JS/script.js
+++ 05-advanced-JS/script.js
@@ -55,3 +55,41 @@ var jane = Object.create(personProto, {
     job: { value: 'designer' }
 });
 */
+
+/**********************
+ * Primitive vs objects
+ ***********************/
+/*
+ // Primitive hold value
+ var a = 23;
+ var b = a;
+ a = 46;
+ console.log(a); // --> 46
+ console.log(b); // --> 23
+ 
+// Object hold reference
+ var obj1 = {
+     name: 'John',
+     age: 26
+ };
+ var obj2 = obj1;
+ obj1.age = 30;
+ console.log(obj1.age); // --> 30
+ console.log(obj2.age); // --> 30
+ 
+ // Functions
+var age = 24;
+var obj = {
+    name: 'Buu',
+    city: 'Sai Gon'
+};
+
+function change(a, b) {
+    a = 30;
+    b.city = 'New York'
+}
+
+change(age, obj);
+console.log(age); // --> 24, because age is a primitive
+console.log(obj.city); // --> New York
+*/
