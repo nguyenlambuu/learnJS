0878c0535f7d9656057ab30ce401209c3b58b637
nguyenlambuu <nguyenlambuu@gmail.com>
2018-09-13 13:51:22 +0700

    Call, Bind, Apply

--- 05-advanced-JS/script.js
+++ 05-advanced-JS/script.js
@@ -246,3 +246,66 @@ interviewQuestionClosures('teacher')('John');
 interviewQuestionClosures('designer')('Jane');
 interviewQuestionClosures('other')('Bob');
 */
+
+/**********************
+ * Call, Bind, Apply
+ ***********************/
+/*
+ var john = {
+	 name: 'John',
+	 age: 1994,
+	 job: 'teacher',
+	 presentation: function(style, timeOfDay){
+		 if(style === 'formal'){
+			console.log('Good ' + timeOfDay + ', Ladies and gentlemen! I\'m ' +  this.name + ', I\'m a ' + this.job + ' and I\'m ' + this.age + ' years old.');			
+		 } else if(style === 'friendly'){
+			console.log('Hey! What\'s up? I\'m ' +  this.name + ', I\'m a ' + this.job + ' and I\'m ' + this.age + ' years old. Have a nice ' + timeOfDay + '.');
+		 }
+	 }
+ }
+
+ var emily = {
+	 name: 'Emily',
+	 age: 35,
+	 job: 'designer'
+ }
+
+ john.presentation('formal', 'morning'); // --> Good morning, Ladies and gentlemen! I'm John, I'm a teacher and I'm 1994 years old.
+
+ // Call
+// ---------
+ john.presentation.call(emily, 'friendly', 'afternoon');
+ // Apply
+// ---------
+ john.presentation.call(emily, ['friendly', 'afternoon']);
+
+// Bind
+// ---------
+var johnFriendly = john.presentation.bind(john, 'friendly');
+johnFriendly('morning');
+johnFriendly('night');
+
+var emilyFormal = john.presentation.bind(emily, 'formal');
+emilyFormal('afternoon');
+
+// Another cool example
+var years = [1990, 1965, 1937, 2005, 1998];
+function arrayCalc(arr, fn) {
+    var arrRes = [];
+    for (var i = 0; i < arr.length; i++) {
+        arrRes.push(fn(arr[i]));
+    }
+    return arrRes;
+}
+function calculateAge(el) {
+    return 2018 - el;
+}
+function isFullAge(limit, el) {
+    return el >= limit;
+}
+
+var ages = arrayCalc(years, calculateAge);
+var fullAgesJapan = arrayCalc(ages, isFullAge.bind(this, 20));
+console.log(ages);
+console.log(fullAgesJapan);
+*/
